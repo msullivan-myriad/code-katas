80a9fb2f4611ffe956fe0af92e8b2c8e48db1b43
Michael Sullivan <msullivan@myriadmobile.com>
2018-10-10 21:18:38 -0500

    Setting up code katas

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const arr = [1, 2, 3];
+
+const somethin = {
+    'value': 123,
+};
+
+class Test {
+
+    constructor(name) {
+       this.name = name;
+    }
+
+    doSomething() {
+       return this.name;
+    }
+
+
+}
+
+const testin = new Test('A name here');
+
+const variable =  testin.doSomething();
+
+console.log(variable);
+
+console.log(arr);
+console.log(somethin);
+
+console.log('Some changes here');
+
+console.log('More stuff here');
+
+console.log('Why is this not logging.......?');
+console.log('Why is this not logging.......?');
+console.log('Why is this not logging.......?');
+
+console.log('And some more stuff!');
+console.log('And some more stuff!');
+console.log('And some more stuff!');
+console.log('And some more stuff!');
+console.log('And some more stuff!');
